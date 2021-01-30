405d22e6d8db947b347a067fdc7c15ecba5aecfc
sushruth17 <sushruth.sushe@gmail.com>
2021-01-30 22:50:19 +0530

    firebase connect + adapter setup

---
+++ app/src/main/java/seventeen/justchat/MainActivity.kt
@@ -0,0 +1,11 @@
+package seventeen.justchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/seventeen/justchat/model/Chat.kt
@@ -0,0 +1,7 @@
+package seventeen.justchat.model
+
+data class Chat(
+    var senderId:String = "",
+    var receiverId:String = "",
+    var message:String = ""
+)
